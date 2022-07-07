728caf2ebef10250da8e6506176288b00ce546ee
Yuri <yuriqpaiva@gmail.com>
2022-07-07 19:22:37 -0300

    Avoid multiple same user creation
    Avoid multiple same user creation at Stripe by using FaunaDB to store customer ID

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,6 +1,17 @@
+import { query as q } from "faunadb";
 import { NextApiResponse, NextApiRequest } from "next";
 import { stripe } from "../../services/stripe";
 import { getSession } from "next-auth/react";
+import { fauna } from "../../services/fauna";
+
+type User = {
+  ref: {
+    id: string;
+  };
+  data: {
+    stripe_customer_id: string;
+  };
+};
 
 export default async function subscribe(
   req: NextApiRequest,
@@ -9,16 +20,34 @@ export default async function subscribe(
   if (req.method === "POST") {
     const session = await getSession({ req });
 
-    const stripeCostumer = await stripe.customers.create(
-      {
-        email: session?.user?.email!,
-        metadata: undefined,
-      },
-      undefined
+    const user = await fauna.query<User>(
+      q.Get(
+        q.Match(q.Index("user_by_email"), q.Casefold(session?.user?.email!))
+      )
     );
 
+    let customerId = user.data.stripe_customer_id;
+
+    if (!customerId) {
+      const stripeCostumer = await stripe.customers.create(
+        {
+          email: session?.user?.email!,
+          metadata: undefined,
+        },
+        undefined
+      );
+
+      await fauna.query(
+        q.Update(q.Ref(q.Collection("users"), user.ref.id), {
+          data: { stripe_customer_id: stripeCostumer.id },
+        })
+      );
+
+      customerId = stripeCostumer.id;
+    }
+
     const stripeCheckoutSession = await stripe.checkout.sessions.create({
-      customer: stripeCostumer.id,
+      customer: customerId,
       payment_method_types: ["card"],
       billing_address_collection: "required",
       line_items: [{ price: "price_1LIcElFbIQ8UQ87GrzvEkjOV", quantity: 1 }],
