610a6c679ee0119f0910403aa1e55cb540dcdca2
Yuri <yuriqpaiva@gmail.com>
2022-07-10 21:39:14 -0300

    Validate user active subscription

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -19,6 +19,35 @@ export default NextAuth({
     }),
   ],
   callbacks: {
+    async session({ session, token, user }) {
+      try {
+
+        // Getting information about user (if has active subscription or not)
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index("subscription_by_user_ref"),
+                q.Select(
+                  "ref",
+                  q.Get(
+                    q.Match(
+                      q.Index("user_by_email"),
+                      q.Casefold(session.user?.email!)
+                    )
+                  )
+                )
+              ),
+              q.Match(q.Index("subscription_by_status"), "active"),
+            ])
+          )
+        );
+
+        return { ...session, activeSubscription: userActiveSubscription };
+      } catch (err) {
+        return { ...session, activeSubscription: null };
+      }
+    },
     async signIn({ user, account, profile }): Promise<boolean> {
       const { email } = user;
 
