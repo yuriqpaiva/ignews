2407e75a45b3f10524740b2cc743de6e5175b56d
Yuri <yuriqpaiva@gmail.com>
2022-07-25 17:59:25 -0300

    Add Home page spec

---
+++ src/tests/pages/Home.spec.tsx
@@ -0,0 +1,25 @@
+import { render, screen } from "@testing-library/react";
+import Home from "../../pages";
+
+jest.mock("next/router", () => {
+  return {
+    useRouter: jest.fn(),
+  };
+});
+
+jest.mock("next-auth/react", () => {
+  return {
+    useSession: () => ({
+      data: null,
+      status: "unauthenticated",
+    }),
+  };
+});
+
+describe("Home page", () => {
+  it("should renders correctly", () => {
+    render(<Home product={{ priceId: "fake-price-id", amount: "R$10,00" }} />);
+
+    expect(screen.getByText("for R$10,00 month")).toBeInTheDocument();
+  });
+});
