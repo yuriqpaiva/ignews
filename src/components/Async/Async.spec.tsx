ab753d8a056ddc1f7177ab43dfb2f4ebb900b5de
Yuri <yuriqpaiva@gmail.com>
2022-07-26 12:29:02 -0300

    Test async components mount

---
+++ src/components/Async/Async.spec.tsx
@@ -0,0 +1,22 @@
+import {
+  render,
+  screen,
+  waitFor,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import { Async } from ".";
+
+test("it renders correctly", async () => {
+  render(<Async />);
+
+  expect(screen.getByText("Hello World")).toBeInTheDocument();
+
+  // // Wait for button render
+  // expect(await screen.findByText("Button", {})).toBeInTheDocument();
+
+  // await waitFor(() => {
+  //   return expect(screen.getByText("Button")).toBeInTheDocument();
+  // });
+
+  await waitForElementToBeRemoved(screen.queryByText("Button"));
+});
